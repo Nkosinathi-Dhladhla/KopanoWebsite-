5f59978175da32ebecedaae21b3e44b2da2a6d89
Nkosinathi-Dhladhla <nkosinathidhladhla77@gmail.com>
2026-01-02 19:27:51 +0200

    Added a scrolling effect using JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const burger = document.getElementById("Burger");
 const burgerImg = document.getElementById("HamburgerLogo");
+const header = document.getElementById("header");
 
 burgerImg.addEventListener("click", function(){
     if(burger.style.display === "none"){
@@ -10,3 +11,27 @@ burgerImg.addEventListener("click", function(){
         burgerImg.src = "Images/hamburger-svgrepo-com.png";
     };
 });
+
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 500){
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+const scroller = document.querySelector(".scroller");
+
+function checkScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+    const scrollerTop = scroller.getBoundingClientRect().top;
+
+    if (scrollerTop < triggerBottom) {
+        scroller.classList.add("visible");
+    } else {
+        scroller.classList.remove("visible");
+    }
+}
+
+window.addEventListener("scroll", checkScroll);
+window.addEventListener("load", checkScroll);
